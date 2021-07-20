2764bf3b2820a1d0b725417cd6d90299eeb99519
Nyota254 <knyota66@gmail.com>
2021-07-20 15:20:39 +0300

    few js array methods

--- main.js
+++ main.js
@@ -27,22 +27,40 @@
 
 
 //Objects
-var cars = {
-    car : "galado",
-    speed : "300kph",
-    drive : '4wd',
-    manufacture : {
-        name : "lamborgini",
-        location : "luxenborg",
-        cartypes : "4"
-    },
-    applyBrakes : function (){
-        setTimeout (function (){
-            console.log("brakes applied");
-        },10000)
-    }
+// var cars = {
+//     car : "galado",
+//     speed : "300kph",
+//     drive : '4wd',
+//     manufacture : {
+//         name : "lamborgini",
+//         location : "luxenborg",
+//         cartypes : "4"
+//     },
+//     applyBrakes : function (){
+//         setTimeout (function (){
+//             console.log("brakes applied");
+//         },10000)
+//     }
     
-}
+// }
 
-console.log(cars.manufacture.name)
-console.log(cars.applyBrakes())
+// console.log(cars.manufacture.name)
+// console.log(cars.applyBrakes())
+
+//Arrays => special types of objects,Positional access of data
+
+//splice method
+//splice()
+//can take several args: 1. where to start working on array
+//                       2.End of where to start working on arrau
+//                       3.From here means it will add something from specified points
+
+ //.concat()
+ //contacts two lists together
+
+ //.sort()
+ //sorts in ascending
+ //.reverse()
+ //sorts in descending order
+
+ 
