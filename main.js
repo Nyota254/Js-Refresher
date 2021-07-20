70489f3159d6e186c8ede686e16058eefeb2d2d0
Nyota254 <knyota66@gmail.com>
2021-07-20 09:43:29 +0300

    js coercion concept

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("working")
+//Some advanced concepts relearning
+
+//Type coercion implicit
+
+//Using operators on number strings it converts
+//e.g 
+
+// console.log(+"2")
+//var value1 = 2>="2"
+//console.log(value1)
+
+//null,undefined,0 all return false below but anything else true
+// if("g"){
+//     console.log("True")
+// } else {
+//     console.log("false")
+// }
